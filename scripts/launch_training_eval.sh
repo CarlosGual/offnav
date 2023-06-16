52e948224bd51dd1d096c5535bd313f1636a0ec3
carlos <carlos.gutierrezalva@uah.es>
2023-06-16 11:59:05 +0200

    prepared for eval good

--- scripts/launch_training_eval.sh
+++ scripts/launch_training_eval.sh
@@ -5,21 +5,18 @@ export MAGNUM_LOG=quiet
 export HABITAT_SIM_LOG=quiet
 
 config="configs/experiments/off_objectnav.yaml"
-DATA_PATH="data/datasets/objectnav/objectnav_hm3d_hd"
-TENSORBOARD_DIR="tb/1/"
+DATA_PATH="data/datasets/objectnav/hm3d/v1"
+TENSORBOARD_DIR="tb/eval/"
 CHECKPOINT_DIR="data/new_checkpoints"
 
 
 echo "In ObjectNav IL DDP"
-python -u -m torch.distributed.launch \
-    --use_env \
-    --nproc_per_node $NUM_GPUS \
-    run.py \
+python -u -m run \
     --exp-config $config \
-    --run-type train \
+    --run-type eval \
     TENSORBOARD_DIR $TENSORBOARD_DIR \
     CHECKPOINT_FOLDER $CHECKPOINT_DIR \
     NUM_UPDATES 50000 \
-    NUM_ENVIRONMENTS 10 \
+    NUM_ENVIRONMENTS 16 \
     RL.DDPPO.force_distributed True \
     TASK_CONFIG.DATASET.DATA_PATH "$DATA_PATH/{split}/{split}.json.gz" \
