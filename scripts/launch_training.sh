8e73095a733b3a6e81e794797e482a43c3f5922a
carlos <carlos.gutierrezalva@uah.es>
2023-06-20 14:44:02 +0200

    trying on DGX with minimal dataset

--- scripts/launch_training.sh
+++ scripts/launch_training.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-export NUM_GPUS=4
+export NUM_GPUS=8
 export GLOG_minloglevel=2
 export MAGNUM_LOG=quiet
 export HABITAT_SIM_LOG=quiet
 
 config="configs/experiments/off_objectnav.yaml"
-DATA_PATH="data/datasets/objectnav/objectnav_hm3d_hd"
-TENSORBOARD_DIR="tb/1/"
+DATA_PATH="data/datasets/objectnav/objectnav_hm3d_hd_minimal"
+TENSORBOARD_DIR="tb/minimal/"
 CHECKPOINT_DIR="data/new_checkpoints"
 
 
@@ -19,7 +19,7 @@ python -u -m torch.distributed.launch \
     --run-type train \
     TENSORBOARD_DIR $TENSORBOARD_DIR \
     CHECKPOINT_FOLDER $CHECKPOINT_DIR \
-    NUM_UPDATES 50000 \
+    NUM_UPDATES 100000 \
     NUM_ENVIRONMENTS 10 \
     RL.DDPPO.force_distributed True \
     TASK_CONFIG.DATASET.DATA_PATH "$DATA_PATH/{split}/{split}.json.gz" \
