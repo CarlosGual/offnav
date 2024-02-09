9c9d82f5ecf8c168dfe6fdab47be6f95be3d9c4a
CarlosGual <carlos.gutierrezalva@uah.es>
2024-02-09 14:20:51 +0100

    trying on DGX with default lr and weight decay

--- scripts/launch_training.sh
+++ scripts/launch_training.sh
@@ -19,9 +19,8 @@ python -u -m torch.distributed.launch \
     --run-type train \
     TENSORBOARD_DIR $TENSORBOARD_DIR \
     CHECKPOINT_FOLDER $CHECKPOINT_DIR \
-    NUM_UPDATES 500000 \
+    NUM_UPDATES 50000 \
     WANDB_ENABLED True \
     NUM_ENVIRONMENTS 4 \
-    WANDB_ENABLED True \
     RL.DDPPO.force_distributed True \
     TASK_CONFIG.DATASET.DATA_PATH "$DATA_PATH/{split}/{split}.json.gz" \
