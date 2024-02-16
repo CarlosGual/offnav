3b754ad557b1a4fc1627e67a9255e0c3a2940fb1
carlos <carlos@gumior.es>
2024-02-16 15:14:12 +0100

    new exp for dgx 2 3

--- offnav/algos/agent.py
+++ offnav/algos/agent.py
@@ -538,8 +538,8 @@ class IQLRNNAgent(nn.Module):
             deterministic_actions = dist.mode().detach().cpu().numpy()
             dataset_actions = actions.detach().cpu().numpy()
             adv = q_pred - vf_pred
-            weighted_beta = (self.beta * inflections_batch).sum(0) / inflections_batch.sum(0)
-            exp_adv = torch.exp(adv * weighted_beta)
+            weighted_adv = (adv * inflections_batch).sum(0) / inflections_batch.sum(0)
+            exp_adv = torch.exp(weighted_adv * self.beta)
             if self.clip_score is not None:
                 exp_adv = torch.clamp(exp_adv, max=self.clip_score)
             weights = exp_adv[:, 0].detach()
