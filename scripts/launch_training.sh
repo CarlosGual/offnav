bd27edbfaba12d3389bac430404b6e2e95e55ee4
CarlosGual <carlos.gutierrezalva@uah.es>
2024-02-28 14:58:58 +0100

    trying on DGX with beta inflection weight on minimal

--- scripts/launch_training.sh
+++ scripts/launch_training.sh
@@ -21,6 +21,6 @@ python -u -m torch.distributed.launch \
     CHECKPOINT_FOLDER $CHECKPOINT_DIR \
     NUM_UPDATES 100000 \
     WANDB_ENABLED True \
-    NUM_ENVIRONMENTS 6 \
+    NUM_ENVIRONMENTS 4 \
     RL.DDPPO.force_distributed True \
     TASK_CONFIG.DATASET.DATA_PATH "$DATA_PATH/{split}/{split}.json.gz" \
