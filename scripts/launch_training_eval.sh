7e9ac03e58753eab7de4d5e0ee640edc123e916a
carlos <carlos@gumior.es>
2024-03-11 17:59:17 +0900

    eval on DGX minival

--- scripts/launch_training_eval.sh
+++ scripts/launch_training_eval.sh
@@ -5,18 +5,18 @@ export MAGNUM_LOG=quiet
 export HABITAT_SIM_LOG=quiet
 
 config="configs/experiments/off_objectnav.yaml"
-DATA_PATH="data/datasets/objectnav/objectnav_hm3d_hd_minimal"
-TENSORBOARD_DIR="tb/small_test"
-CHECKPOINT_DIR="data/small_test"
-
+DATA_PATH="data/datasets/objectnav/objectnav_hm3d_hd"
+TENSORBOARD_DIR="tb/small_test_minival"
+CHECKPOINT_DIR="data/from_scratch_beta_3_with_weighting_and_lr_3e6_minimal"
 
 echo "In ObjectNav IL DDP"
 python -u -m run \
     --exp-config $config \
     --run-type eval \
     TENSORBOARD_DIR $TENSORBOARD_DIR \
-    CHECKPOINT_FOLDER $CHECKPOINT_DIR \
+    EVAL_CKPT_PATH_DIR $CHECKPOINT_DIR \
     NUM_UPDATES 50000 \
     NUM_ENVIRONMENTS 8 \
     RL.DDPPO.force_distributed True \
+    EVAL.SPLIT "mini_val" \
     TASK_CONFIG.DATASET.DATA_PATH "$DATA_PATH/{split}/{split}.json.gz" \
