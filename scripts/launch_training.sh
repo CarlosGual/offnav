35c7f59c5f617871ba451a46b6eca681553e0535
carlos <carlos@gumior.es>
2024-03-18 17:31:09 +0900

    go back to best hyper for the moment

--- scripts/launch_training.sh
+++ scripts/launch_training.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-export NUM_GPUS=5
+export NUM_GPUS=3
 export GLOG_minloglevel=2
 export MAGNUM_LOG=quiet
 export HABITAT_SIM_LOG=quiet
 
 config="configs/experiments/off_objectnav.yaml"
 DATA_PATH="data/datasets/objectnav/objectnav_hm3d_hd"
-TENSORBOARD_DIR="tb/from_scratch_full_dataset_3e4_wd"
-CHECKPOINT_DIR="data/from_scratch_full_dataset_3e4_wd"
+TENSORBOARD_DIR="tb/fsfd_3e4_wd"
+CHECKPOINT_DIR="data/fsfd_3e4_wd"
 
 
 echo "In ObjectNav OFFNAV"
