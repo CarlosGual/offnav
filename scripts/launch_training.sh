a566ae2d265034a0f541c2a2977a2a26cf12313e
carlos <carlos@gumior.es>
2024-03-25 20:22:56 +0900

    launch on DGX setup3 exp

--- scripts/launch_training.sh
+++ scripts/launch_training.sh
@@ -24,6 +24,6 @@ python -u -m torch.distributed.launch \
     CHECKPOINT_FOLDER $CHECKPOINT_DIR \
     NUM_UPDATES 200000 \
     WANDB_ENABLED True \
-    NUM_ENVIRONMENTS 28 \
+    NUM_ENVIRONMENTS 26 \
     RL.DDPPO.force_distributed True \
     TASK_CONFIG.DATASET.DATA_PATH "$DATA_PATH/{split}/{split}.json.gz" \
