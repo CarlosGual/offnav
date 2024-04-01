639386c49b76ac29548b26d0f5c99a345f82681c
carlos <carlos@gumior.es>
2024-04-01 16:59:20 +0900

    launch full training

--- scripts/launch_training.sh
+++ scripts/launch_training.sh
@@ -24,7 +24,7 @@ python -u -m torch.distributed.launch \
     CHECKPOINT_FOLDER $CHECKPOINT_DIR \
     NUM_UPDATES 200000 \
     WANDB_ENABLED True \
-    NUM_ENVIRONMENTS 32 \
-    OFFLINE.IQL.num_mini_batch 8 \
+    NUM_ENVIRONMENTS 20 \
+    OFFLINE.IQL.num_mini_batch 5 \
     RL.DDPPO.force_distributed True \
     TASK_CONFIG.DATASET.DATA_PATH "$DATA_PATH/{split}/{split}.json.gz" \
